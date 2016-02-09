e18b55f3702514c42b4ca46887055a169c93db57
andiro12 <andriola@365.is>
2016-02-09 17:40:18 +0000

    templates

--- js/index.js
+++ js/index.js
@@ -7,10 +7,23 @@ var myViewModel = function() {
 
   // This observable array initially contains three objects
   self.anotherObservableArray = ko.observableArray([
-      { name: "Bungle", type: "Bear" },
-      { name: "George", type: "Hippo" },
-      { name: "Zippy", type: "Unknown" }
+    { name: "Bungle", type: "Bear" },
+    { name: "George", type: "Hippo" },
+    { name: "Zippy", type: "Unknown" }
   ]);
+
+  self.people = ko.observableArray([
+    { name: 'Franklin', credits: 250 },
+    { name: 'Mario', credits: 5800 }
+  ]);
+
+  self.seasons = ko.observableArray([
+    { name: 'Spring', months: [ 'March', 'April', 'May' ] },
+    { name: 'Summer', months: [ 'June', 'July', 'August' ] },
+    { name: 'Autumn', months: [ 'September', 'October', 'November' ] },
+    { name: 'Winter', months: [ 'December', 'January', 'February' ] }
+  ]);
+
 };
 
 $(document).ready(function() {
